638af7f3e35c8bcced5df643f5126f3d9580935f
Gaia De Donatis <gaia.de-donatis3@etu.univ-lorraine.fr>
2024-11-06 13:36:48 +0100

    Log In Activity

--- app/src/main/java/com/upm/androidnewsletter/LoginActivity.java
+++ app/src/main/java/com/upm/androidnewsletter/LoginActivity.java
@@ -1,25 +1,72 @@
 package com.upm.androidnewsletter;
 
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // components
+        usernameEditText = findViewById(R.id.main_username);
+        passwordEditText = findViewById(R.id.main_password);
+        loginButton = findViewById(R.id.main_btn_log_in);
+
+        // SharedPreferences to save API key
+        sharedPreferences = getSharedPreferences("LoginPrefs", MODE_PRIVATE);
+
+        // Login button
+        loginButton.setOnClickListener(view -> {
+            String username = usernameEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
+            new LoginTask().execute(username, password);
         });
+    }
+
+    // Login asynchronous
+    private class LoginTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String username = params[0];
+            String password = params[1];
+
+            // fake token
+            if ("user".equals(username) && "password".equals(password)) {
+                return "fakeApiKey123456";  // Return a fake API key if login is successful
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String apiKey) {
+            if (apiKey != null) {
+                // save API key in SharedPreferences
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("apikey", apiKey);
+                editor.apply();
+
+                // if Success
+                Toast.makeText(LoginActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
+
+                // startActivity(new Intent(LoginActivity.this, ShowArticlesActivity.class));
+            } else {
+                // if Error
+                Toast.makeText(LoginActivity.this, "Invalid credentials", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
